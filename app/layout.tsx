36c8a2cd93870fe2b8a99670fb41f0c2af1024c6
Ítalo <italo.varzah@gmail.com>
2024-01-23 10:59:36 -0300

    feat: adding static and dynamic SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,18 @@ import { Header } from './components/header'
 import { ContactForm } from './components/contact-form'
 import { Footer } from './components/footer'
 
+export const metadata = {
+  title: {
+    default: 'Home',
+    template: '%s | IPV',
+  },
+  icons: [
+    {
+      url: '/favicon.svg',
+    },
+  ],
+}
+
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
@@ -20,10 +32,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
       <body>
-        <Header/>
+        <Header />
         {children}
-        <ContactForm/>
-        <Footer/>
+        <ContactForm />
+        <Footer />
       </body>
     </html>
   )
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { ProjectDetails } from '@/app/components/pages/project/project-details'
 import { ProjectsSections } from '@/app/components/pages/project/project-sections'
 import { ProjectPageData, ProjectsPageStaticData } from '@/app/types/page-info'
 import { fetchHygraphQuery } from '@/app/utils/fetch-hygraph-query'
+import { Metadata } from 'next'
 
 type ProjectProps = {
   params: {
@@ -69,4 +70,23 @@ export async function generateStaticParams() {
   const { projects } = await fetchHygraphQuery<ProjectsPageStaticData>(query)
 
   return projects
+}
+
+export async function generateMetadata({
+  params: { slug },
+}: ProjectProps): Promise<Metadata> {
+  const data = await getProjectDetails(slug)
+  const project = data.project
+
+  return {
+    title: project.title,
+    description: project.description.text,
+    openGraph: {
+      images: {
+        url: project.thumbnail.url,
+        width: 1200,
+        height: 630,
+      },
+    },
+  }
 }
